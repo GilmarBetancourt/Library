b4aea5cc5be64532fc3cd6efb76b4e81f302ae40
gilmarbeta <gilmarbeta@hotmail.com>
2023-04-21 16:55:15 -0400

    Added function to add information to the book cover.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Library
+
+This is a simple library to keep track of your books for the Odin Project.
--- index.js
+++ index.js
@@ -32,18 +32,31 @@ function createBook(){
     newBook.style.backgroundImage = "url('Images/bookcover.png')";
     booksDiv.appendChild(newBook);   
     writeBookCover(); 
+    eraseForm();
 
 }
 
 function writeBookCover(){
     let newCover = new Book(newTitle.value, newAuthor.value, newPages.value, newStatus.value);
     let newInfo = document.createElement("div");
+    newInfo.classList.add("bookCover");
     let text = "";
     for (let x in newCover){
         text += newCover[x] + "<br>";
     }
     newInfo.innerHTML = text;
     newBook.appendChild(newInfo);
+}
+
+function eraseForm(){
+    const formFields = document.querySelectorAll(".formInput");
+    for(let i = 0; i<formFields.length; i++){
+        formFields[i].value = "";
+        //FIX: This is not working
+        if(formFields[i].nodeType == "checkbox"){
+            formFields[i].ariaChecked  == false;
+        }
+    }
 }
 
 
